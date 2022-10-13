e32b0409ec6709ad72d15d8dbdf8f1ac95b2d03b
Kesh Ramjith <kesh.ramjith@protonmail.com>
2022-10-13 21:10:14 +0200

    added spring data jpa, and postgres. Added database properties. Added annotations to User object. Created UserRepository. Used UserRepository in UserController to use database. Postgres instance running in Docker container locally.

--- src/main/java/com/example/atuserservice/data/User.java
+++ src/main/java/com/example/atuserservice/data/User.java
@@ -2,10 +2,17 @@ package com.example.atuserservice.data;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity(name = "Users")
 public class User {
-   // TODO: some form of ID, going to use Postgres
-    String id;
-    String email;
-    String password;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    public void setId(Long id) {
+        this.id = id;
+    }
+    private String email;
+    private String password;
 }
--- src/main/java/com/example/atuserservice/web/UserController.java
+++ src/main/java/com/example/atuserservice/web/UserController.java
@@ -1,30 +1,49 @@
 package com.example.atuserservice.web;
 
 import com.example.atuserservice.data.User;
+import com.example.atuserservice.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/user")
 public class UserController {
 
-    // just to check with POSTMAN
-    @GetMapping
-    public ResponseEntity<ArrayList<User>> getAllUsers() {
-        return new ResponseEntity<>(new ArrayList<User>(), HttpStatus.OK);
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
+    // just to check with POSTMAN
     @GetMapping
-    public ResponseEntity<String> getUser(@RequestParam String id) {
-        return new ResponseEntity<>(id, HttpStatus.OK);
+    public ResponseEntity<Iterable<User>> getAllUsers() {
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<User>> getUser(@RequestParam Long id) {
+        return new ResponseEntity<>(userRepository.findById(id), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
-        return new ResponseEntity<>(user, HttpStatus.CREATED);
+        return new ResponseEntity<>(userRepository.save(user), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> putUser(@PathVariable Long id, @RequestBody User user) {
+        return (userRepository.existsById(id)) ?
+                new ResponseEntity<>(userRepository.save(user), HttpStatus.OK)
+                : new ResponseEntity<>(userRepository.save(user), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
     }
 }
